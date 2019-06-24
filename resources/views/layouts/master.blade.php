3da5d014eee5e7e2590c7947f87d0ac070415556
Chondrima <chondrima.xponent@gmail.com>
2019-06-24 16:18:20 +0600

    first commit upto half 4 part

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Laravel Ecommerce Project</title>
+
+        @include('partials.styles')
+
+    </head>
+    <body>
+    <div class="wrapper">
+
+        @include('partials.nav')
+
+        @yield('content')
+
+        @include('partials.footer')
+        
+    </div>
+
+    @include('partials.scripts')
+
+    </body>
+</html>
